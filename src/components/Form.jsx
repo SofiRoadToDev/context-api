91b9216dcaa0809709c14708041bec9f012abac7
Sofi <sofi@gmail.com>
2023-06-12 12:55:57 -0300

    Modifying form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,15 +4,15 @@ import TodoContext from '../app-data/TodoContext'
 
 const Form = () => {
 
-  const todo={
+  const Emptytodo={
     title:'',
     description:'',
     completed:false
   }
 
   const{createToDo}=useContext(TodoContext)
-  const[todoTitle,setTodoTitle]=useState('')
-  const[completed,setCompleted]=useState(false)
+  const[todo,setTodo]=useState(Emptytodo)
+  
 
 
 
@@ -22,21 +22,32 @@ const Form = () => {
       console.log(checked);
       if(type==='text')
       {
-        [name]=value
+        setTodo({
+          ...todo,
+          [name]:value
+        })
+        
       }
-      //setTodoTitle(value)
-      if(type==='checkbox')setCompleted(checked)
+      if(type=='textarea'){
+        setTodo({
+          ...todo,
+          description:value
+        })
+      }
+      
+      if(type==='checkbox')
+      setTodo({
+        ...todo,
+        completed:checked
+      })
      
   }
 
   const handleSubmit=(e)=>{
     e.preventDefault()
-    createToDo({
-      title:todoTitle,
-      completed
-    })
-    setTodoTitle('')
-    setCompleted(false)
+    createToDo({todo})
+    setTodo(Emptytodo)
+   
     
   }
 
@@ -47,17 +58,17 @@ const Form = () => {
     <form >
         <p className="h3 text-light text-center">Create a new Task to do</p>
         <label htmlFor="title" className="form-label text-light pt-2">Título del To Do</label>
-        <input type="text" name="todoTitle" id="title" className="form-control " onChange={handleChange} value={todoTitle} />
+        <input type="text" name="title" id="title" className="form-control " onChange={handleChange} value={todo.title} />
         
         <label htmlFor="desc" className='form-label text-light'>Description</label>
-        <textarea id="desc" name="description" className='form-control' rows="5" value={todo.description}/>
+        <textarea id="desc" name="description" className='form-control' rows="5" onChange={handleChange} value={todo.description}/>
 
         <label htmlFor="img" className="form-label text-light">Imagen</label>
         <input type="file" id="img" name="file" className='form-control' />
 
         <div className="form-check form-switch m-2 text-light w-100">
-          <input id="completed" type="checkbox"className='form-check-input' onChange={handleChange} name="completed" checked={completed} />
-          <label htmlFor="completed">{completed?'Completa':'Incompleta'}</label>
+          <input id="completed" type="checkbox"className='form-check-input' onChange={handleChange} name="completed" checked={todo.completed} />
+          <label htmlFor="completed">{todo.completed?'Completa':'Incompleta'}</label>
         </div>
 
 
